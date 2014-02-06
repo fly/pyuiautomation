62babe5358d74d820fa9db6097d8487ee010c738
fly <fly@codeconst.com>
2014-02-06 15:56:48 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyuiautomation',
+    version='1.0.0',
+    description='Python realisation of basic Microsoft UI Automation functionality',
+    py_modules=['pyuiautomation'],
+    requires=['comtypes']
+)
